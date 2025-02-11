b317405e26cb98d17a9944074a41ee688b8c2eb7
Debjyoti Ghosh <54670671+debjyoti1712@users.noreply.github.com>
2025-02-12 00:00:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 Created on Tue Feb  4 16:22:34 2025
 
 @author: debjyoti
+
+ref: https://www.youtube.com/watch?v=43RJ3JByygE&list=PL2VXyKi-KpYtZzm1K8UKnnBzsOCtekhKq
 """
 
 import streamlit as st
@@ -18,26 +20,26 @@ gross_income = st.number_input("Enter your gross salary : ")
 if st.button("Calculate"):   
     if gross_income > 1275000:
         taxable_gross_income = gross_income - 75000
-    if taxable_gross_income > 2400000:
-        tax = 400000*0.75  + (taxable_gross_income - 2400000)*0.3
-    elif taxable_gross_income > 2000000:
-        tax = 400000*0.5 + (taxable_gross_income - 2000000)*0.25
-    elif taxable_gross_income > 1600000:
-        tax = 400000*0.3 + (taxable_gross_income - 1600000)*0.2
+        if taxable_gross_income > 2400000:
+            tax = 400000*0.75  + (taxable_gross_income - 2400000)*0.3
+        elif taxable_gross_income > 2000000:
+            tax = 400000*0.5 + (taxable_gross_income - 2000000)*0.25
+        elif taxable_gross_income > 1600000:
+            tax = 400000*0.3 + (taxable_gross_income - 1600000)*0.2
+        else:
+            tax = 400000*0.15 + (taxable_gross_income - 1200000)*0.15
+        
+        st.write("\n==================================================")
+        st.write("\nStandard deduction (INR): 75000")
+        st.write (f"\nYearly taxable gross income (after SD) is (INR): {taxable_gross_income}")
+        st.write("\n==================================================")
+        st.write (f"\nYearly net tax payable is (INR): {tax}")
+        st.write (f"\nMonthly net tax payable is (INR): {tax/12}")
+        st.write("\n==================================================")
+        st.write (f"\nMonthly in-hand approximate salary (INR): {(taxable_gross_income-tax)/12}")
+        st.write("\n==================================================")
     else:
-        tax = 400000*0.15 + (taxable_gross_income - 1200000)*0.15
-    
-    st.write("\n==================================================")
-    st.write("\nStandard deduction (INR): 75000")
-    st.write (f"\nYearly taxable gross income (after SD) is (INR): {taxable_gross_income}")
-    st.write("\n==================================================")
-    st.write (f"\nYearly net tax payable is (INR): {tax}")
-    st.write (f"\nMonthly net tax payable is (INR): {tax/12}")
-    st.write("\n==================================================")
-    st.write (f"\nMonthly in-hand approximate salary (INR): {(taxable_gross_income-tax)/12}")
-    st.write("\n==================================================")
-else:
-    st.write("\n==================================================")
-    st.write (f"\nMonthly tentative in-hand salary (INR): {gross_income/12}")
-    st.write("\nHurray ! No tax to be paid...")
-    st.write("\n==================================================")
+        st.write("\n==================================================")
+        st.write (f"\nMonthly tentative in-hand salary (INR): {gross_income/12}")
+        st.write("\nHurray ! No tax to be paid...")
+        st.write("\n==================================================")
